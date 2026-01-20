8b93403159bb7062744d96d5e5021cb4699599e7
itsmemarwyn <91468656+itsmemarwyn@users.noreply.github.com>
2026-01-20 08:19:12 +0000

    Task 1: Create a new Odoo module

---
+++ addons_library/library_extensions/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Library Extensions",
+    "version": "1.0",
+    "depends": ["library"],
+    "category": "Extra Tools",
+    "summary": "Extension for Library Module",
+    "description": "This module is an extension of the library module.",
+    "author": "Engr. Marwyn C. Galindo",
+    "data": [],
+    "installable": True,
+    "application": False,
+}
